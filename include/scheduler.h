91a8ff0fb0bfe084ce6b2754a24d688dc64aa119
hc-tec <2173324540@qq.com>
2022-04-11 00:47:01 +0800

    feat: scheduler perfect

--- include/scheduler.h
+++ include/scheduler.h
@@ -5,13 +5,213 @@
 #ifndef TIT_COROUTINE_SCHEDULER_H
 #define TIT_COROUTINE_SCHEDULER_H
 
+#include <cstring>
+
+#include <functional>
+#include <vector>
+
+#include "base/mutex.h"
+
+#include "table.h"
+#include "context/context.h"
+
 namespace tit {
 
 namespace co {
 
-class scheduler {
+using Mutex = base::MutexLock;
+using MutexGuard = base::MutexLockGuard;
+
+struct Coroutine;
+
+class Scheduler {
+ public:
+  typedef std::function<void()> Func;
+
+  void go(Func);
+
+ protected:
+  Scheduler() = default;
+  ~Scheduler() = default;
+};
+
+class TaskManager {
+ public:
+  TaskManager() = default;
+  ~TaskManager() = default;
+
+  void add_new_task(Scheduler::Func* cb) {
+    MutexGuard guard(mutex_);
+    new_tasks_.push_back(cb);
+  }
+
+  void add_ready_task(Coroutine* co) {
+    MutexGuard guard(mutex_);
+    ready_tasks_.push_back(co);
+  }
+
+  void get_all_tasks(
+      std::vector<Scheduler::Func*>& new_tasks,
+      std::vector<Coroutine*>& ready_tasks
+  ) {
+    MutexGuard guard(mutex_);
+    if (!new_tasks_.empty()) new_tasks_.swap(new_tasks);
+    if (!ready_tasks_.empty()) ready_tasks_.swap(ready_tasks);
+  }
+
+ private:
+    Mutex mutex_;
+    std::vector<Scheduler::Func*> new_tasks_;
+    std::vector<Coroutine*> ready_tasks_;
+};
+
+struct Stack {
+  char* p;       // stack pointer
+  char* top;     // stack top
+  Coroutine* co; // coroutine owns this stack
+};
+
+
+
+
+
+enum co_state_t : uint8_t {
+  st_init = 0,     // initial state
+  st_wait = 1,     // wait for an event
+  st_ready = 2,    // ready to resume
+  st_timeout = 4,  // timeout
+};
+
+struct Coroutine {
+  Coroutine();
+  ~Coroutine();
+
+  uint32_t id;
+  uint8_t state;
+  uint8_t sid;
+
+  tb_context_t ctx;
+
+  // for saving stack data of this coroutine
+  union { char stack[1024*1024]; char _dummy1[sizeof(stack)]; };
+
+  // Once the coroutine starts, we no longer need the cb, and it can
+  // be used to store the Scheduler pointer.
+  union {
+    Scheduler::Func* cb;   // coroutine function
+    Scheduler* s;  // scheduler this coroutine runs in
+  };
 
 };
+
+class Copool {
+ public:
+  // _tb(14, 14) can hold 2^28=256M coroutines.
+  Copool() : table_(14, 14), id_(0) {
+    ids_.reserve(1u << 14);
+  }
+
+  ~Copool() {
+    for (int i = 0; i < id_; ++i) table_[i].~Coroutine();
+  }
+
+  Coroutine* pop() {
+    if (!ids_.empty()) {
+      int last = ids_.back();
+      ids_.pop_back();
+      auto& co = table_[last];
+      co.state = st_init;
+      co.ctx = 0;
+      memset(co.stack, 0, 1024 * 1024);
+      return &co;
+    } else {
+      auto& co = table_[id_];
+      co.id = id_++;
+      co.sid = (uint8_t)(co.id & 7);
+      return &co;
+    }
+  }
+
+  void push(Coroutine* co) {
+    ids_.push_back(co->id);
+    if (ids_.size() >= 1024) memset(co->stack, 0, 1024 * 1024);
+  }
+
+  Coroutine* operator[](size_t i) {
+    return &table_[i];
+  }
+
+ private:
+  co::table<Coroutine> table_;
+  std::vector<int> ids_;
+  int id_;
+};
+
+
+class SchedulerImpl : public Scheduler {
+ public:
+  SchedulerImpl(int id, int sched_num, int stack_size);
+  ~SchedulerImpl();
+
+  Coroutine* running() { return running_co_; }
+
+  void resume(Coroutine* co);
+
+  void yield();
+
+  // add a new task will run in a coroutine later (thread-safe)
+  void add_new_task(Scheduler::Func* cb) {
+    task_mgr_.add_new_task(cb);
+//    _epoll->signal();
+  }
+
+  // add a coroutine ready to resume (thread-safe)
+  void add_ready_task(Coroutine* co) {
+    task_mgr_.add_ready_task(co);
+//    _epoll->signal();
+  }
+
+ private:
+
+  static void main_func(tb_context_from_t from);
+
+  void recycle() {
+    stack_[running_co_->sid].co = 0;
+    co_pool_.push(running_co_);
+  }
+
+  // save stack for the coroutine
+  void save_stack(Coroutine* co) {
+    if (co) {
+      co->stack.clear();
+      co->stack.append(co->ctx, stack_[co->sid].top - (char*)co->ctx);
+    }
+  }
+
+  // pop a Coroutine from the pool
+  Coroutine* new_coroutine(Scheduler::Func* cb) {
+    Coroutine* co = co_pool_.pop();
+    co->cb = cb;
+    return co;
+  }
+
+  int wait_ms_;
+  int id_;
+  int sched_num_;
+  int stack_size_;
+  Stack* stack_;
+
+  Coroutine* running_co_;
+  Coroutine* main_co_;
+
+  Copool co_pool_;
+  TaskManager task_mgr_;
+
+  bool stop_;
+
+};
+
+
 
 }  // namespace co
 
--- src/scheduler.cc
+++ src/scheduler.cc
@@ -4,11 +4,79 @@
 
 #include "scheduler.h"
 
+#include <cstdlib>
+
+#include <iostream>
 
 namespace tit {
 
 namespace co {
 
+__thread SchedulerImpl* gSched = 0;
+
+void Scheduler::go(Func) {
+
+}
+
+SchedulerImpl::SchedulerImpl(int id, int sched_num, int stack_size)
+    : wait_ms_((uint32_t)-1),
+      id_(id),
+      sched_num_(sched_num),
+      stack_size_(stack_size),
+      running_co_(0),
+      co_pool_(),
+      stop_(false) {
+
+    stack_ = (Stack*) calloc(8, sizeof(Stack));
+    main_co_ = co_pool_.pop(); // coroutine with zero id is reserved for _main_co
+}
+
+void SchedulerImpl::main_func(tb_context_from_t from) {
+  ((Coroutine*)from.priv)->ctx = from.ctx;
+  (*gSched->running()->cb)(); // run the coroutine function
+  tb_context_jump(from.ctx, 0); // jump back to the from context
+}
+
+void SchedulerImpl::resume(Coroutine* co) {
+  tb_context_from_t from;
+  Stack* s = &stack_[co->sid];
+  running_co_ = co;
+  if (s->p == 0) {
+    s->p = (char*) malloc(stack_size_);
+    s->top = s->p + stack_size_;
+    s->co = co;
+  }
+
+  if (co->ctx == 0) {
+    // resume new coroutine
+    if (s->co != co) { this->save_stack(s->co); s->co = co; }
+    co->ctx = tb_context_make(s->p, stack_size_, main_func);
+    std::cout << "resume new co: " << co << " id: " << co->id << std::endl;
+    from = tb_context_jump(co->ctx, main_co_); // jump to main_func(from):  from.priv == _main_co
+
+  } else {
+
+    // resume suspended coroutine
+    std::cout << "resume co: " << co << ", id: " <<  co->id << ", stack: " << co->stack.size() << std::endl;
+    if (s->co != co) {
+      this->save_stack(s->co);
+      CHECK(s->top == (char*)co->ctx + co->stack.size());
+      memcpy(co->ctx, co->stack.data(), co->stack.size()); // restore stack data
+      s->co = co;
+    }
+    from = tb_context_jump(co->ctx, main_co_); // jump back to where the user called yiled()
+  }
+
+  if (from.priv) {
+    // yield() was called in the coroutine, update context for it
+    assert(running_co_ == from.priv);
+    running_co_->ctx = from.ctx;
+    std::cout << "yield co: " << running_co_ << " id: " << running_co_->id << std::endl;
+  } else {
+    // the coroutine has terminated, recycle it
+    this->recycle();
+  }
+}
 
 
 }  // namespace co
