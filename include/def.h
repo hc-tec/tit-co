efef365d54191b6f65c5da8290a4ab59d00d4080
hc-tec <2173324540@qq.com>
2022-04-11 23:11:23 +0800

    perf: add branch optimization

---
+++ include/def.h
@@ -0,0 +1,11 @@
+//
+// Created by titto on 2022/4/11.
+//
+
+#ifndef TIT_COROUTINE_DEF_H
+#define TIT_COROUTINE_DEF_H
+
+#define  unlikely(x)  __builtin_expect(!!(x), 0)
+#define  likely(x)  __builtin_expect(!!(x), 1)
+
+#endif  // TIT_COROUTINE_DEF_H
