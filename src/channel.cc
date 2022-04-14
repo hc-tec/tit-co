1381343643e94639207beb429d984b09d7c62bcd
hc-tec <2173324540@qq.com>
2022-04-14 12:14:49 +0800

    feat: channel write finish

--- src/channel.cc
+++ src/channel.cc
@@ -56,6 +56,9 @@ void ChannelImpl::Read(void *p) {
       scheduler->Yield();
 
       // TODO: if timeout
+      if (wait_ctx->buf_ != p) {
+        memcpy(p, wait_ctx->buf_, blk_size_);
+      }
     } else {
       memcpy(p, buf_ + rx_, blk_size_);
       rx_ += blk_size_;
@@ -78,6 +81,54 @@ void ChannelImpl::Read(void *p) {
   }
 }
 
+void ChannelImpl::Write(const void* p) {
+  mutex_.Lock();
+  if (wx_ == rx_) {
+    memcpy(buf_ + wx_, p, blk_size_);
+    wx_ += blk_size_;
+    if (wx_ == buf_size_) wx_ = 0;
+    if (wx_ == rx_) full_ = true;
+  } else {
+    if (!full_) {
+      while (!wait_queue_.empty())  {
+        WaitCtx::Ptr wait_ctx = wait_queue_.front();
+        wait_queue_.pop();
+        if (atomic_compare_swap(&wait_ctx->state_, kInit, kReady) == kInit) {
+          mutex_.UnLock();
+          memcpy(wait_ctx->buf_, p, blk_size_);
+          Coroutine* wait_co = wait_ctx->co_;
+          static_cast<SchedulerImpl*>(wait_co->scheduler_)->AddReadyTask(wait_co);
+          return;
+        }
+      }
+      memcpy(buf_ + wx_, p, blk_size_);
+      wx_ += blk_size_;
+      if (wx_ == buf_size_) wx_ = 0;
+      if (wx_ == rx_) full_ = true;
+      mutex_.UnLock();
+    } else {
+      SchedulerImpl* scheduler = SchedulerTLS::instance();
+      Coroutine* co = scheduler->running();
+
+      WaitCtx::Ptr wait_ctx = CreateWaitCtxInfo(co, const_cast<void*>(p));
+      if (wait_ctx->buf_ != p) {
+        memcpy(wait_ctx->buf_, p, blk_size_);
+      }
+      wait_queue_.push(wait_ctx);
+      mutex_.UnLock();
+
+      co->wait_ctx_ = wait_ctx;
+
+      if (co->scheduler_ != scheduler) co->scheduler_ = scheduler;
+
+      // TODO: Add Timer
+
+      scheduler->Yield();
+
+      // TODO: if timeout
+    }
+  }
+}
 
 }  // namespace co
 
