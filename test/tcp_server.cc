8a9b16077dfd520c510b17328f6d6b188a7cca4b
hc-tec <2173324540@qq.com>
2022-04-22 00:06:07 +0800

    test: modify tcp_server test, add delegate test

--- test/tcp_server.cc
+++ test/tcp_server.cc
@@ -16,12 +16,32 @@
 
 using namespace tit;
 
+class ServerHandler : public co::TcpServer::Delegate {
+ public:
+  void OnBind(const co::TcpSocket::Ptr &server_sock) override {
+    LOG(INFO) << "Bind";
+  }
+  void OnListen(const co::TcpSocket::Ptr &server_sock) override {
+    LOG(INFO) << "Listen";
+  }
+  void OnNewConn(const co::TcpSocket::Ptr &new_sock) override {
+    LOG(INFO) << "New Connection";
+  }
+  void OnMessage(const co::TcpSocket::Ptr &new_sock, const char *buffer,
+                 uint len) override {
+
+  }
+  void OnSendFinish(const co::TcpSocket::Ptr &new_sock, const char *buffer,
+                    uint len) override {
+
+  }
+};
 
 int main () {
 
-  co::IPv4Address::Ptr addr = co::IPv4Address::Create("127.0.0.1", 8888);
+  co::IPv4Address::Ptr addr = co::IPv4Address::Create("127.0.0.1", 8889);
   co::TcpServer server(addr);
-
+  server.set_delegate(new ServerHandler());
   server.Start();
 
   char ch;
