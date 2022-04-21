55be489bd53dce464e5422ed0c975ba20ed2c624
hc-tec <2173324540@qq.com>
2022-04-21 14:02:54 +0800

    feat: io event make socket adapt to coroutine possible

--- include/io_event.h
+++ include/io_event.h
@@ -5,6 +5,9 @@
 #ifndef TIT_COROUTINE_IO_EVENT_H
 #define TIT_COROUTINE_IO_EVENT_H
 
+#include "def.h"
+#include "base/noncopyable.h"
+
 namespace tit {
 
 namespace co {
@@ -13,7 +16,25 @@ enum io_event_t {
   kEvRead = 1,
   kEvWrite = 2,
 };
-class IOEvent {};
+
+class IOEvent {
+ public:
+  IOEvent(int fd, io_event_t ev)
+      : fd_(fd),
+        ev_(ev),
+        has_ev_(false) {}
+
+  ~IOEvent();
+
+  bool wait(uint32 ms = -1);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(IOEvent);
+
+  int fd_;
+  io_event_t ev_;
+  bool has_ev_;
+};
 
 }  // namespace co
 
--- src/io_event.cc
+++ src/io_event.cc
@@ -3,11 +3,38 @@
 //
 
 #include "io_event.h"
+
+#include "scheduler.h"
+
 namespace tit {
 
 namespace co {
 
+IOEvent::~IOEvent() {
+  auto& scheduler = TLSScheduler::instance();
+  if (has_ev_) scheduler->DelIoEvent(fd_, ev_);
+}
 
+bool IOEvent::wait(uint32 ms) {
+  auto& scheduler = TLSScheduler::instance();
+  if (!has_ev_) {
+    has_ev_ = scheduler->AddIoEvent(fd_, ev_);
+    if (!has_ev_) return false;
+  }
+  if (ms != -1) {
+    scheduler->AddTimer(ms);
+    scheduler->Yield();
+    if (!scheduler->is_timeout()) {
+      return true;
+    } else {
+      errno = ETIMEDOUT;
+      return false;
+    }
+  } else {
+    scheduler->Yield();
+    return true;
+  }
+}
 
 }  // namespace co
 
