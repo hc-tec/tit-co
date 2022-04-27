056404ea02f21654dec64cd8325143c4840dd0b9
hc-tec <2173324540@qq.com>
2022-04-27 23:58:05 +0800

    refactor: add debug info and fix out of bound

--- src/rpc/session.cc
+++ src/rpc/session.cc
@@ -14,8 +14,9 @@ static constexpr int8 kDataLen = 4;
 static constexpr int16 kDataMaxLen = 10000;
 
 Protocol::Ptr RpcSession::RecvProtocol() {
-
-  char data_len_[kDataLen];
+  LOG(DEBUG) << "recv protocol";
+  char data_len_[kDataLen+1];
+  data_len_[kDataLen] = '\0';
   if(socket_->Recvn(data_len_, kDataLen, -1) < kDataLen) {
     return nullptr;
   }
@@ -24,18 +25,20 @@ Protocol::Ptr RpcSession::RecvProtocol() {
 
   LOG(DEBUG) << "packet length: " << data_len;
 
-  char buf[data_len];
+  char buf[data_len+1];
+  buf[data_len] = '\0';
   if(socket_->Recvn(buf, data_len, -1) < data_len) {
     return nullptr;
   }
-  std::string data(buf);
 
+  std::string data(buf);
+  LOG(DEBUG) << "recv protocol buf data: " << buf;
   Protocol::Ptr protocol = BaseProtocolSerializer::Deserialize(data);
-  LOG(DEBUG) << protocol->toString();
   return protocol;
 }
 
 bool RpcSession::SendProtocol(const Protocol::Ptr& protocol) {
+  LOG(DEBUG) << "send protocol";
   std::string buf = BaseProtocolSerializer::Serialize(protocol);
   int32 buf_len = buf.size();
   if (buf_len > kDataMaxLen) {
@@ -43,10 +46,13 @@ bool RpcSession::SendProtocol(const Protocol::Ptr& protocol) {
     return false;
   }
   std::string data_len = std::to_string(buf_len);
+
   if (socket_->Send(data_len.data(), kDataLen, -1) < kDataLen) {
     LOG(DEBUG) << "packet length send error";
     return false;
   }
+  LOG(DEBUG) << "send protocol buf size: " << buf_len;
+  LOG(DEBUG) << "send protocol buf data: " << buf.data();
   if (socket_->Send(buf.data(), buf_len, -1) < buf_len) {
     LOG(DEBUG) << "packet body send error";
     return false;
