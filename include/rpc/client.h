dc7857f3e5db7f60ff315ebd11a3ca2b6d4da1d9
hc-tec <2173324540@qq.com>
2022-04-28 18:04:46 +0800

    feat: add Call method in rpc client

--- include/rpc/client.h
+++ include/rpc/client.h
@@ -16,6 +16,7 @@
 #include "atomic.h"
 #include "net/socket.h"
 #include "net/address.h"
+#include "protocol_manager.h"
 
 namespace tit {
 
@@ -48,15 +49,21 @@ class RpcClient {
 
   bool Connect(const Address::Ptr& addr);
 
-//  template <class TargetProtocol>
-  using TargetProtocol = Protocol;
-  Result<Protocol, TargetProtocol> Call(const std::string& svr_name, const typename TargetProtocol::Ptr& target_proto) {
+  template <char const* srv_proto>
+  Result<typename SvrName2Protocol<srv_proto>::REQ,
+         typename SvrName2Protocol<srv_proto>::RES>
+      Call(const std::string& srv_name,
+       const typename SvrName2Protocol<srv_proto>::REQ::Ptr& req_proto) {
+    using REQProtocol = typename SvrName2Protocol<srv_proto>::REQ;
+    using RESProtocol = typename SvrName2Protocol<srv_proto>::RES;
+    using REQProtocolPtr = typename REQProtocol::Ptr;
+    using RESProtocolPtr = typename RESProtocol::Ptr;
 
-    Serializer<TargetProtocol> serializer;
-    std::string target_data = serializer.Serialize(target_proto);
+    Serializer<REQProtocol> serializer;
+    std::string target_data = serializer.Serialize((ProtocolInterface*)req_proto);
 
     Protocol::Ptr base_proto = Protocol::Create(MsgType::kRequestCall, req_id_, target_data);
-
+    base_proto->set_srv_name(srv_name);
     send_channel_ << base_proto;
 
     Channel<Protocol::Ptr> recv_channel_(1, params_.recv_timeout_);
@@ -69,11 +76,20 @@ class RpcClient {
 
     Protocol::Ptr recv_proto;
     recv_channel_ >> recv_proto;
+
     if (timeout()) {
-      Result<Protocol, TargetProtocol> res;
+      Result<REQProtocol , RESProtocol> res;
       return res.Timeout();
     }
-    Result<Protocol, TargetProtocol> res(base_proto, recv_proto);
+
+    Serializer<RESProtocol> res_serializer;
+    RESProtocolPtr res_proto = (RESProtocolPtr) res_serializer.Deserialize(recv_proto->data());
+
+    if (res_proto == nullptr) {
+      Result<REQProtocol , RESProtocol> res;
+      return res.NoMethod();
+    }
+    Result<REQProtocol , RESProtocol> res(req_proto, res_proto);
     return res.Success();
   }
 
